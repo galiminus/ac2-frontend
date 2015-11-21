ef848371d58d2af34473fbe9e19cd52582e2c618
Victor Goya <victor.goya@precogs.fr>
2015-11-21 11:56:24 +0100

    persist tokens and currentUser in localStorage

--- source/store.js
+++ source/store.js
@@ -1,9 +1,29 @@
 import { createStore, applyMiddleware, compose } from 'redux'
-import reducer from "reducers"
+import Immutable from "immutable"
+import rootReducer from "reducers"
 import thunk from 'redux-thunk'
+import persistState, {mergePersistedState} from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage/adapter';
+import filter from 'redux-localstorage-filter';
+
+const reducer = compose(
+  mergePersistedState((state, persistedState) => {
+    state.tokens = Immutable.fromJS(persistedState.tokens)
+    state.currentUser = persistedState.currentUser
+    return state
+  })
+)(rootReducer);
+
+const storage = compose(
+  filter(["tokens", "currentUser"])
+)(adapter(window.localStorage));
 
 let createStoreWithMiddleware = compose(
-  applyMiddleware(thunk)
+  applyMiddleware(thunk),
 )(createStore)
 
-export default createStoreWithMiddleware(reducer)
+const createPersistentStore = compose(
+  persistState(storage, 'state')
+)(createStoreWithMiddleware);
+
+export default createPersistentStore(reducer)
