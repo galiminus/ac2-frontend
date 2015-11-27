4a5bb8d0e4efe80bec55a45a64cf9bc79945b124
Victor Goya <victor.goya@precogs.fr>
2015-11-27 13:22:27 +0100

    add a reducer for posts and use it in feeds

--- source/components/feed.js
+++ source/components/feed.js
@@ -2,10 +2,15 @@ import React from "react"
 import { connect } from 'react-redux'
 
 import {
+  Card,
+  CardText,
+  CardHeader
 } from "material-ui"
 
 function mapStateToProps(state) {
-  return {}
+  return {
+    posts: state.posts
+  }
 }
 
 function mapDispatchToProps(dispatch) {
@@ -13,21 +18,28 @@ function mapDispatchToProps(dispatch) {
 }
 
 let Feed = function(props) {
-  let filling;
-  for (let i = 0; i < 100; i++) {
-    filling += `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-    nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-    culpa qui officia deserunt mollit anim id est laborum`
-  }
+  const postCards = props.posts.map(post =>
+    <Card key={i} style={{marginTop: 32}}>
+      <CardHeader title="Title" />
+      <CardText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+        culpa qui officia deserunt mollit anim id est laborum
+      </CardText>
+    </Card>
+  )
+
   return (
     <div {...props}>
-      <img style={{width: "100%"}} src="https://placeholdit.imgix.net/~text?txtsize=33&txt=placeholder&w=1000&h=300" />
+      <img style={{width: "100%"}} src="" />
       <div className="container-fluid">
-        {filling}
+        <div className="col-md-10 col-md-offset-1 col-xs-12">
+          {postCards}
+        </div>
       </div>
     </div>
   );
--- source/reducers.js
+++ source/reducers.js
@@ -2,16 +2,20 @@ import { combineReducers } from 'redux'
 import { reducer as form } from 'redux-form'
 import { routeReducer } from 'redux-simple-router'
 
-import tokens from "reducers/tokens"
 import users from "reducers/users"
 import currentUser from "reducers/current-user"
+
+import tokens from "reducers/tokens"
 import currentToken from "reducers/current-token"
+
+import posts from "reducers/posts"
 
 let reducers = combineReducers({
     tokens,
     currentToken,
     users,
     currentUser,
+    posts,
     form,
     routing: routeReducer
   }
