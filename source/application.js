ae2f2bd56f31ff7f27350e4c11725f7f81d7b6e7
Victor Goya <victor.goya@precogs.fr>
2015-12-03 17:18:08 +0100

    I'm so lame at picking colors >< but at least now we can use our own theme

--- source/application.js
+++ source/application.js
@@ -6,6 +6,11 @@ import ReactDOM from "react-dom"
 
 import { Router, Route } from 'react-router'
 
+import ThemeManager from 'material-ui/lib/styles/theme-manager'
+import Colors from 'material-ui/lib/styles/colors'
+import ColorManipulator from 'material-ui/lib/utils/color-manipulator'
+import Spacing from 'material-ui/lib/styles/spacing'
+
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 import { syncReduxAndRouter } from 'redux-simple-router'
 
@@ -27,18 +32,49 @@ import messages from "messages/fr-FR"
 const history = createBrowserHistory()
 syncReduxAndRouter(history, store)
 
-ReactDOM.render(
-  <Provider store={store}>
-    <IntlProvider locale="fr" messages={messages}>
-      <Router history={history}>
-        <Route path="/welcome" component={WelcomePage}>
-          <Route path="login" component={LoginForm} />
-          <Route path="recover" component={RecoverForm} />
-          <Route path="signup" component={SignupForm} />
-        </Route>
-        <Route path="/" component={HomePage} />
-      </Router>
-    </IntlProvider>
-  </Provider>
-  ,
-document.getElementsByTagName("main")[0])
+const Application = React.createClass({
+  childContextTypes : {
+    muiTheme: React.PropTypes.object,
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getMuiTheme({
+        spacing: Spacing,
+        fontFamily: 'Roboto, sans-serif',
+        palette: {
+          primary1Color: Colors.indigo400,
+          primary2Color: Colors.cyan700,
+          primary3Color: Colors.grey400,
+          accent1Color: Colors.orangeA700,
+          accent2Color: Colors.grey100,
+          accent3Color: Colors.grey500,
+          textColor: Colors.darkBlack,
+          alternateTextColor: Colors.white,
+          canvasColor: Colors.white,
+          borderColor: Colors.grey300,
+          disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3)
+        }
+      })
+    };
+  },
+
+  render() {
+    return (
+      <Provider store={store}>
+        <IntlProvider locale="fr" messages={messages}>
+          <Router history={history}>
+            <Route path="/welcome" component={WelcomePage}>
+              <Route path="login" component={LoginForm} />
+              <Route path="recover" component={RecoverForm} />
+              <Route path="signup" component={SignupForm} />
+            </Route>
+            <Route path="/" component={HomePage} />
+          </Router>
+        </IntlProvider>
+      </Provider>
+    )
+  }
+})
+
+ReactDOM.render(<Application />, document.getElementsByTagName("main")[0])
