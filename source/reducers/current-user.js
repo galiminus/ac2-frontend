e9dfa7613f1521fb9f6220a75fbf127d7b72cef9
Victor Goya <victor.goya@precogs.fr>
2015-12-06 00:29:14 +0100

    remove all user infos on disconnect

--- source/reducers/current-user.js
+++ source/reducers/current-user.js
@@ -4,6 +4,8 @@ export default function(state = null, action) {
   switch (action.type) {
     case "SET_CURRENT_USER":
       return state = String(action.id)
+    case "REMOVE_ALL_TOKENS":
+      return null
     default:
       return state
   }
--- source/reducers/users.js
+++ source/reducers/users.js
@@ -4,6 +4,8 @@ export default function(state = Immutable.Map({}), action) {
   switch (action.type) {
     case "ADD_USER":
       return state.set(String(action.data.id), action.data)
+    case "REMOVE_ALL_TOKENS":
+      return Immutable.Map({})
     default:
       return state
   }
