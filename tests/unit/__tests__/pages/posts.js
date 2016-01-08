ae460da47a8944b02a18e7bb8f19cc2ce17045f5
Victor Goya <victor.goya@precogs.fr>
2016-01-08 01:56:00 +0100

    add everything to start testing

---
+++ tests/unit/__tests__/pages/posts.js
@@ -0,0 +1,13 @@
+jest.dontMock("pages/posts");
+
+const React = require("react");
+const ReactDOM = require("react-dom");
+const TestUtils = require("react-addons-test-utils");
+
+const PostsPages = require("pages/posts");
+const RaisedButton = require("material-ui").RaisedButton;
+
+describe("PostsPages", () => {
+    it("should works", () => {
+    });
+});
