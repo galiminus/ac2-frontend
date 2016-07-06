5c5ae7a9d1d03e4537a33915d554f944c3ba83a5
Victor Goya <phorque@furlab.in>
2016-07-07 01:36:41 +0200

    use welcome page illustration from server

--- source/components/welcome/welcome.js
+++ source/components/welcome/welcome.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 import CSSModules from 'react-css-modules';
 import styles from './welcome.css';
@@ -10,15 +11,32 @@ import ToolbarLogo from 'components/toolbar-logo';
 import Notifier from 'components/notifier';
 
 function mapStateToProps(state) {
-    return {
+    const props = {
         translation: state.translations.get(state.currentLocale)
     };
+
+    if (state.settings && state.settings.data) {
+        props.welcomePageIllustrations = state.settings.data.welcomePageIllustrations;
+    }
+
+    return (props);
 }
+
+const defaultProps = {
+    welcomePageIllustrations: []
+};
 
 const WelcomePage = React.createClass({
     propTypes: {
         children: PropTypes.object.isRequired,
-        translation: PropTypes.object.isRequired
+        translation: PropTypes.object.isRequired,
+        welcomePageIllustrations: PropTypes.array.isRequired
+    },
+
+    mixins: [PureRenderMixin],
+
+    getDefaultProps() {
+        return (defaultProps);
     },
 
     render() {
@@ -29,8 +47,7 @@ const WelcomePage = React.createClass({
                         <ToolbarLogo />
                     </ToolbarGroup>
                 </Toolbar>
-                <div styleName="illustration">
-
+                <div styleName="illustration" style={{ backgroundImage: this.props.welcomePageIllustrations[0] }}>
                 </div>
                 <div styleName="formContainer">
                     <div styleName="formPaper">
